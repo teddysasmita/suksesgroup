62720d3ab73a80436597b398e90888c4061bc1c3
Teddy Sasmita <teddie.sasmita@gmail.com>
2014-04-26 09:38:32 +0700

    Ok boss

--- protected/components/Accounting.php
+++ protected/components/Accounting.php
@@ -27,29 +27,66 @@
  * Revenue = Pendapatan = PD
  * ------------------
  * Expense = Pengeluaran (Biaya) = BY
- * Depreciation Expense = Pengeluaran Depresiasi = 
+ * Depreciation Expense = Pengeluaran Depresiasi = PDR
  * ------------------
- * Interest Revenues = Pendapatan dari Bunga
+ * Interest Revenues = Pendapatan dari Bunga = PBU
  * ------------------
- * Cost = Biayas
+ * Cost = Biaya =
 */
 
 class Accounting {
 	
-	public static createChartEntry($idadmin, $prefixname, $kind, $level = 1 )
+	public $account_name = array(
+		"KT"=>"Kas Di Tangan",
+		"KB"=> "Kas Di Bank",
+		"IBD"=> "Persediaan Barang Dagang",
+		"APR"=> "Peralatan",
+		"AT"=> "Tanah",
+		"AB"=> "Bangunan",
+		"AS"=> "Persediaan",
+		"DAB"=> "Depresiasi Bangunan",
+		"DAPR"=> "Depresiasi Peralatan",
+		"ASR"=> "Asuransi",
+		"APT"=> "Piutang",
+		"LH"=> "Hutang",
+		"LHU"=> "Hutang Usaha",
+		"LHH"=> "Hutang Upah",
+		"LHB"=> "Bunga Hutang",
+		"LUR"=> "Uang Muka (Titipan)",
+		"LHT"=> "Hipotek",
+		"MD"=> "Modal",
+		"RV"=> "Pendapatan Usaha",
+		"EB"=> "Pengeluaran",
+		"EDR"=> "Pengeluaran Depresiasi",
+		"RBU"=> "Pendapatan dari Bunga",
+	);
+		
+	public static function createChartEntry($idadmin, $kind, $level = 1 )
 	{
 		$data['idref'] = $idadmin;
 		$data['kind'] = $kind;
 		$data['level'] = $level;
 		$data['id'] = idmaker::getCurrentID2();
-		$data['userlog'] = Yii->app()->user->id;
+		$data['userlog'] = Yii::app()->user->id;
 		$data['datetimelog'] = idmaker::getDateTime();
-		$data['name'] = $prefixname;
 		
 		Yii::app()->db->createCommand()->insert('raccounts', $data);
+	}
 	
+	public static function getChartEntry($idadmin, $kind)
+	{
+		$result = Yii::app()->db->createCommand()
+			->select('id')->from('raccounts')
+			->where('idref = :p_idref and kind = :p_kind',
+				array(':p_kind'=>$kind, ':p_idref'=>$idref) )
+			->queryScalar();
+	}
 	
-	
+	public static function deleteChartEntry($idadmin)
+	{
+		Yii::app()->db->createCommand()
+			->delete('raccounts', 'idadmin = :p_idadmin',
+				array('p_idadmin'=>$idadmin));
 	}
 }
 
